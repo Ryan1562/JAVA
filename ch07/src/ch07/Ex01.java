257db9edd63ff3be9dd20379accfe7dd40a9b922
user <user@DESKTOP-H3PHNB1>
2022-07-08 17:12:00 +0900

    .

--- ch07/src/ch07/Ex01.java
+++ ch07/src/ch07/Ex01.java
@@ -1,5 +1,23 @@
 package ch07;
-
+class Product {
+	int price; // 제품의 가격
+	int bonusPoint; // 제품구매 시 제공하는 보너스점수
+//	public Product() {	} // 생성자가 없으면 자바 커파일러가 default 생성자를 만들어 준다
+	Product(int price) {  // 생성자가 하나라도 있으면 default생성자를 안 만들어 준다
+		this.price = price;
+		bonusPoint = (int) (price / 10.0);
+	}
+}
+class Tv extends Product {
+	Tv() {	
+		super(100);  // 매개변수를 전수를 가진 생성자 호출
+	} // default 생성자를 가진 부모를 호출하는 데 부모가 default생성자 없어서 에러
+	public String toString() {
+		return "Tv";
+	}
+}
 public class Ex01 {
-
+	public static void main(String[] args) {
+		Tv t = new Tv();
+	}
 }
--- ch07/src/ch07/Ex02.java
+++ ch07/src/ch07/Ex02.java
@@ -1,5 +1,27 @@
 package ch07;
-
+class Parent {
+	int x = 100; // 200으로 변경 
+	Parent() {	// 3
+		this(200); 	
+	}
+	Parent(int x) { // 4
+		this.x = x; // 5
+	}
+	int getX() {	return x;	}
+}
+class Child extends Parent {
+	int x = 3000;  // 1000으로 변경
+	Child() {	
+		this(1000); // 1	
+	} 
+	Child(int x) {	
+		super(); // 2 생략 된것으로 인식
+		this.x = x;
+	}
+}
 public class Ex02 {
-
+	public static void main(String[] args) {
+		Child c = new Child();
+		System.out.println("x=" + c.getX());
+	}
 }
